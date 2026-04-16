845ec995d4f5df050802c9e2460d83ea455093b9
vikasbishnoi <vikasbishnoi82289@gmail.com>
2026-04-16 10:20:29 +0530

    Initial Commit

---
+++ GymBuddyTests/GymBuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  GymBuddyTests.swift
+//  GymBuddyTests
+//
+//  Created by Vikas Bishnoi on 16/04/26.
+//
+
+import Testing
+@testable import GymBuddy
+
+struct GymBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
